c47e6f5d0be39d50cd431b68c84afad2d3a801b5
LinneaMay <Linneamay@gilbertson.net>
2025-03-01 13:33:32 -0600

    added observer patterns with MenuServer and Customers as observers

--- src/Customers/Customer.java
+++ src/Customers/Customer.java
@@ -1,8 +1,13 @@
 package Customers;
 import java.util.ArrayList;
 
-public abstract class Customer {
+public abstract class Customer implements Observer{
     // protected ArrayList<Ingredients> bannedFoods;
+    protected ArrayList<String> MenuItems;
+
+    public void update(ArrayList<String> meals) {
+        MenuItems = meals;
+    }
 
     public abstract void getMenu();
     
---
+++ src/Menu/Server.java
@@ -0,0 +1,9 @@
+import Customers.Customer;
+
+public interface Server {
+
+    public void registerObserver(Customer customer);
+    public void removeObserver(Customer customer);
+    public void notifyObservers();
+
+}
