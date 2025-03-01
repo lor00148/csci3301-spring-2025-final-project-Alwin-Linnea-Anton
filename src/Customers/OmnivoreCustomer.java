bb3e91d74386d88ab909b57a3c7cfacd3601d5a1
Anton Olson <ols00559@morris.umn.edu>
2025-03-01 13:55:56 -0600

    Added Menu implementations for Omnivore, Vegan, and Vegetarian Customers

--- src/Customers/OmnivoreCustomer.java
+++ src/Customers/OmnivoreCustomer.java
@@ -2,6 +2,10 @@ package Customers;
 public class OmnivoreCustomer extends Customer{
     @Override
     public void getMenu() {
-        
+        System.out.println("Menu for " + name + ":");
+        for(int i = 0; i < menu.foods.size(); i++)
+        {
+            System.out.println(menu.foods.get(i));
+        }
     }
 }
